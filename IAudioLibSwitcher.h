c3ffd095b393208c2b9a3be78c8bd2c1a036b196
Rasmus Anthin <scratty@gmail.com>
2024-03-28 09:09:04 +0100

    Using unsigned int as handles instead of int as that is what OpenAL supports. Subject to change in the future but we're using OpenAL as template for now.

--- IAudioLibSwitcher.h
+++ IAudioLibSwitcher.h
@@ -20,36 +20,36 @@ namespace audio
     
     virtual void finish() = 0;
   
-    virtual int create_source() = 0;
+    virtual unsigned int create_source() = 0;
     
-    virtual void destroy_source(int src_id) = 0;
+    virtual void destroy_source(unsigned int src_id) = 0;
     
-    virtual int create_buffer() = 0;
+    virtual unsigned int create_buffer() = 0;
     
-    virtual void destroy_buffer(int buf_id) = 0;
+    virtual void destroy_buffer(unsigned int buf_id) = 0;
     
-    virtual void play_source(int src_id) = 0;
+    virtual void play_source(unsigned int src_id) = 0;
     
-    virtual bool is_source_playing(int src_id) = 0;
+    virtual bool is_source_playing(unsigned int src_id) = 0;
     
-    virtual void pause_source(int src_id) = 0;
+    virtual void pause_source(unsigned int src_id) = 0;
     
-    virtual void stop_source(int src_id) = 0;
+    virtual void stop_source(unsigned int src_id) = 0;
     
-    virtual void set_source_volume(int src_id, float vol) = 0;
+    virtual void set_source_volume(unsigned int src_id, float vol) = 0;
     
-    virtual void set_source_pitch(int src_id, float pitch) = 0;
+    virtual void set_source_pitch(unsigned int src_id, float pitch) = 0;
     
-    virtual void set_source_looping(int src_id, bool loop) = 0;
+    virtual void set_source_looping(unsigned int src_id, bool loop) = 0;
     
     // #NOTE: Might be deprecated.
-    virtual void detach_source(int src_id) = 0;
+    virtual void detach_source(unsigned int src_id) = 0;
     
-    virtual void set_source_standard_params(int src_id) = 0;
+    virtual void set_source_standard_params(unsigned int src_id) = 0;
     
-    virtual void set_buffer_data_mono_16(int buf_id, const std::vector<short>& buffer, int sample_rate) = 0;
+    virtual void set_buffer_data_mono_16(unsigned int buf_id, const std::vector<short>& buffer, int sample_rate) = 0;
     
-    virtual void attach_buffer_to_source(int src_id, int buf_id) = 0;
+    virtual void attach_buffer_to_source(unsigned int src_id, unsigned int buf_id) = 0;
     
     virtual std::string check_error() = 0;
   };
